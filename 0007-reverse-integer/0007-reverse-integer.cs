c1276ffa9380463eb57955893cf4d50bd91f0a64
dotnetbenji <dotnetbenji@gmail.com>
2025-12-17 20:29:08 +0000

    Time: 27 ms (48.13%), Space: 29.3 MB (47.38%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cs
+++ 0007-reverse-integer/0007-reverse-integer.cs
@@ -3,25 +3,22 @@ public class Solution {
 
         int sign = x < 0 ? -1 : 1;
 
-        try
-        {
-            int pos = Math.Abs(x);
-
-            int reverse = 0;
-            while(pos > 0)
-            {
-                checked
-                {
-                    reverse = reverse * 10 + (pos % 10);
-                }
-                pos /= 10;
-            }
-
-            return reverse * sign;
-        }
-        catch(OverflowException)
-        {
+        if(x == int.MinValue)
             return 0;
+
+        int pos = Math.Abs(x);
+
+        int reverse = 0;
+        while(pos > 0)
+        {
+            int digit = pos % 10;
+            if (reverse > (int.MaxValue - digit) / 10)
+                return 0;
+
+            reverse = reverse * 10 + digit;
+            pos /= 10;
         }
+
+        return reverse * sign;
     }
 }
