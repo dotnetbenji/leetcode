90b35da1d59661b8bc2a6b554fbf919afabd04a2
dotnetbenji <dotnetbenji@gmail.com>
2025-12-17 18:19:42 +0000

    Time: 1 ms (27.12%), Space: 50.7 MB (77.61%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> visited = new();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int num = nums[i];
+            int required = target - num;
+
+            if(!visited.TryGetValue(required, out int index))
+            {
+                visited.TryAdd(num, i);
+                continue;
+            }
+            else
+            {
+                return new int[] { index+1, i+1 };
+            }
+        }
+
+        return [];
+    }
+}
