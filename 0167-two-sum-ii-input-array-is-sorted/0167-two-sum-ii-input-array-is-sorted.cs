9778a35ac8736e30991f48a4b5e5e1b9ef9147de
dotnetbenji <dotnetbenji@gmail.com>
2025-12-17 19:48:11 +0000

    Time: 8 ms (11.45%), Space: 50.7 MB (85.74%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cs
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cs
@@ -1,22 +1,46 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int, int> visited = new();
         for(int i = 0; i < nums.Length; i++)
         {
-            int num = nums[i];
-            int required = target - num;
+            int required = target - nums[i];
 
-            if(!visited.TryGetValue(required, out int index))
-            {
-                visited.TryAdd(num, i);
+            int reqTargetIndex = BinSearch(nums, required, i);
+
+            if(reqTargetIndex == -1)
                 continue;
-            }
-            else
-            {
-                return new int[] { index+1, i+1 };
-            }
+
+            return reqTargetIndex > i
+                ? new int[] {i+1, reqTargetIndex+1}
+                : new int[] {reqTargetIndex+1, i+1};
         }
 
         return [];
+    }
+
+    public int BinSearch(int[] nums, int target, int firstIndex) 
+    {
+        int index = nums.Length/2;
+        int min = 0;
+        int max = nums.Length-1;
+
+        while(nums[index] != target || index == firstIndex)
+        {
+            if(target > nums[index])
+            {
+                min = index;
+                index += (int)Math.Ceiling((max-index)/2f);
+                if(index == min)
+                    return -1;
+            }
+            else
+            {
+                max = index;
+                index -= (int)Math.Floor((index-min)/2f);
+                if(index == max)
+                    return -1;
+            }
+        }
+
+        return index;
     }
 }
