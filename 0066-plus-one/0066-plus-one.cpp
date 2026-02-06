718ec172aa5479056068b521075e871ff4fd4d27
dotnetbenji <dotnetbenji@gmail.com>
2026-02-06 17:50:44 +0000

    Time: 0 ms (100%), Space: 11.7 MB (17.41%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -8,11 +8,6 @@ public:
             return digits;
         }
 
-        if(len == 1 && digits[0] == 9)
-        {
-            return vector<int> {1, 0};
-        }
-
         for(int i = len-1; i > 0; i--)
         {
             if(digits[i-1] < 9)
