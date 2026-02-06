a97dee7259b651d3da12b8e570ca4ee41216526b
dotnetbenji <dotnetbenji@gmail.com>
2026-02-06 17:42:17 +0000

    Time: 0 ms (100%), Space: 11.3 MB (90.86%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int len = digits.size();
+        if(digits.back() < 9)
+        {
+            digits[len-1]++;
+            return digits;
+        }
+
+        if(len == 1 && digits[0] == 9)
+        {
+            return vector<int> {1, 0};
+        }
+
+        for(int i = len-1; i > 0; i--)
+        {
+            if(digits[i-1] < 9)
+            {
+                digits[i-1]++;
+                for(int j = i; j < len; j++)
+                {
+                    digits[j] = 0;
+                }
+                return digits;
+            }
+        }
+
+        digits.insert(digits.begin(), 1);
+        for(int i = 1; i < len+1; i++)
+        {
+            digits[i] = 0;
+        }
+        return digits;
+    }
+};
